2b26ce951cf667fe590dd280335af3eb444f8983
kanterbury <ksuzuki.db@gmail.com>
2023-05-21 10:25:51 +0900

    replace card name to link

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import cardSchema from "../schemas/cardSchema";
-
+import Link from "next/link";
 
 const cardsSchema = z.object({
   contents: z.array(cardSchema),
@@ -30,7 +30,9 @@ export default async function Page() {
       <h1>Cards</h1>
       <li className="list-none">
         {cards.contents.map((card) => (
-          <ul key={card.id}>{card.name}</ul>
+          <ul key={card.id}>
+            <Link href={`/cards/${card.id}`}>{card.name}</Link>
+          </ul>
         ))}
       </li>
     </>
